c0dc070a38956ada402d7b714edfd1b048ff382a
Sehar-Tahir <sehartahirofficial@gmail.com>
2025-07-14 10:00:15 +0500

    frontend url updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
@@ -16,10 +16,10 @@ connectDB();
 
 
 // app.use(cors({ origin: allowedOrigins, credentials: true }));
-app.use(cors({
-  origin: "https://mern-auth-frontend-ecru.vercel.app",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "https://mern-auth-frontend-ecru.vercel.app",
+//   credentials: true,
+// }));
 // app.options("*", cors({
 //   origin: "https://mern-auth-frontend-ecru.vercel.app",
 //   credentials: true,
@@ -30,7 +30,10 @@ app.use(cookieParser());
 
 
 //Api Endpoints - Routes
-app.get('/', (req, res) => {res.send("Hello From Server")})
+// app.get('/', (req, res) => {res.send("Hello From Server")})
+app.get('/', (req, res) => {
+  res.redirect(process.env.FRONTEND_URL);
+})
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
