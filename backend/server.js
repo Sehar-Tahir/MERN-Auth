8d9f237b81c01b856742d7f05db3b151d395c282
Sehar-Tahir <sehartahirofficial@gmail.com>
2025-07-14 09:23:14 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js'
 import userRouter from './routes/userRoutes.js';
 
+
 const app = express();
 const port = process.env.PORT || 4000
 connectDB();
